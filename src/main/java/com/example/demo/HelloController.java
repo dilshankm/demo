658442e8e0424c0bb0835bb09bbb5e8b95b8dbc4
dilshankm <dilshankm@outlook.com>
2020-10-08 23:24:19 +0530

    hello

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,16 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	
-	@PostMapping(path = "/hello")
-	public String hello() {
-		return "hello";
+
+	@PostMapping(path = "/hello/{hello}")
+	public String hello(@PathVariable String hello) {
+		System.out.println(hello);
+		return "hello " + hello;
 	}
 
 }
--- src/main/java/com/example/demo/HelloFilter.java
+++ src/main/java/com/example/demo/HelloFilter.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -9,10 +10,13 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
+@Component
 public class HelloFilter extends GenericFilterBean{
 
 	@Override
@@ -22,11 +26,15 @@ public class HelloFilter extends GenericFilterBean{
 		HttpServletResponse res = (HttpServletResponse) response;
 		ContentCachingRequestWrapper conReq = new ContentCachingRequestWrapper(req);
 		ContentCachingResponseWrapper conRes = new ContentCachingResponseWrapper(res);
-		chain.doFilter(req, res);
+		
 		int i = 2;
 		if(i!=1) {
+			Map m =(Map) req.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+			System.out.println("yooo");
+			System.out.println(m.get("hello"));
 			conRes.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
 		}
+		chain.doFilter(req, res);
 		conRes.copyBodyToResponse();
 	}
 
